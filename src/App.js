4de9741b9fbc557a613de65ca361cb55e883012d
SahilThakur7 <sahilrd07@gmail.com>
2017-07-06 16:49:33 +0530

    Another commit

--- src/App.js
+++ src/App.js
@@ -6,46 +6,56 @@ class App extends React.Component {
         this.state = {
             txt: "this is the state text",
             cat: 0,
-            currentEvent: '---'
+            currentEvent: '---',
+            a: ''
         }
         this.update2 = this.update2.bind(this)
     }
     update(e) {
-        this.setState({txt: e.target.value})
+        this.setState({txt: this.refs.b.value, a: this.a.refs.input.value})
     }
     update2(e) {
         this.setState({currentEvent: e.type})
     }
     render(){
         return(
-                <div>
-                <input type="text" onChange={this.update.bind(this)}/>
-                <h1>{this.state.txt}</h1>
-                <h2>{this.props.txt}</h2>
-                <p><Button>I<Heart/>React</Button></p>
-                <p><Title text="Karanveer"/></p>
-                <textarea onKeyPress={this.update2} onCopy={this.update2}/>
+            <div>
+            <input type="text" ref="b" onChange={this.update.bind(this)}/>
+            <h1>{this.state.txt}</h1>
+            <h2>{this.props.txt}</h2>
+            <p><Button>I<Heart/>React</Button></p>
+            <p><Title text="Karanveer"/></p>
+            <textarea onKeyPress={this.update2} onCopy={this.update2}/>
+            <h2>{this.state.currentEvent}</h2>
+            <div>
+            <Input update={this.update.bind(this)} ref={ component => this.a = component} /> {this.state.a}
+            </div>
 
-                <h2>{this.state.currentEvent}</h2>
-                </div>
-              )
+            </div>
+        )
     }
 }
 const Button = (props) => <button>{props.children}</button>
 
-class Heart extends React.Component {
-    render() {
-        return <span>&hearts;</span>
+    class Heart extends React.Component {
+        render() {
+            return <span>&hearts;</span>
+        }
     }
-}
 
 const Title = (props) => <h1>Raju &hearts; {props.text}</h1>
-Title.propTypes = {
-    text(props, propName, component){
-        if (props[propName].length < 6) {
-            return new Error('Length of ${propName}')
-        }
+    Title.propTypes = {
+        text(props, propName, component){
+            if (props[propName].length < 6) {
+                return new Error('Length of ${propName}')
+            }
 
+        }
+    }
+
+class Input extends React.Component {
+    render(){
+        return <div><input type="text" ref="input" onChange={this.props.update}/></div>
     }
 }
 
