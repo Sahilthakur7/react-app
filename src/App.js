31318c03e6ed4bd2cf8f21c0b43f9d54824765c0
SahilThakur7 <sahilrd07@gmail.com>
2017-07-21 17:56:20 +0530

    StarWars API(array used)

--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ class App extends React.Component {
             </div>
 
             <button onClick={this.update3}>{this.state.val * this.state.m }</button>
+            <StarWars/>
             </div>
         )
     }
@@ -97,4 +98,40 @@ class Wrapper extends React.Component {
             </div>)
     }
 }
+
+class StarWars extends React.Component {
+    constructor(){
+        super();
+        this.state = { items: []}
+    }
+    componentWillMount() {
+        fetch('http://swapi.co/api/people/?format=json')
+        .then( response => response.json() )
+        .then ( ({results: items}) => this.setState({items}))
+
+    }
+    filter(e){
+        this.setState({filter: e.target.value})
+    }
+    render(){
+        let items = this.state.items
+        if(this.state.filter){
+            items = items.filter( item => 
+            item.name.toLowerCase()
+            .includes(this.state.filter.toLowerCase()))
+
+        }
+        return (
+            <div>
+                <input type="text" onChange={this.filter.bind(this)}/>
+                {items.map(item => 
+                    <Person key={item.name} person={item}/>)}
+            </div>
+        )
+    }
+}
+
+const Person = (props) => <h4>{props.person.name}</h4>
 export default Wrapper
+
+
