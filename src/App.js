734ee99e1ab2f8f0cbbb4709efef0f87fef8a800
SahilThakur7 <sahilrd07@gmail.com>
2017-07-23 16:16:29 +0530

    Higher order components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const HOC = (InnerComponent) => class extends React.Component {
+    constructor(){
+        super();
+        this.state = {count: 0}
+    }
+    update(){
+        this.setState({count: this.state.count + 1})
+    }
+    componentWillMount(){
+        console.log("HOC mounted")
+    }
+    render(){
+        return(
+            <InnerComponent {...this.props} {...this.state} update= {this.update.bind(this)}/>
+        )
+    }
+}
+
 class App extends React.Component {
     constructor(){
         super();
@@ -54,6 +72,9 @@ class App extends React.Component {
 
             <button onClick={this.update3}>{this.state.val * this.state.m }</button>
             <StarWars/>
+            <Button2>Dbao mujhe</Button2>
+            <br/>
+            <LabelHOC>Dont touch me</LabelHOC>
             </div>
         )
     }
@@ -131,6 +152,20 @@ class StarWars extends React.Component {
     }
 }
 
+const Button2 = HOC((props) => <button onClick={props.update}>{props.children}-{props.count}</button>)
+
+class Label extends React.Component {
+    componentWillMount(){
+        console.log("Label mounted")
+    }
+    render(){
+        return(
+            <label onMouseMove = {this.props.update}>{this.props.children}- {this.props.count}</label>
+        )
+    }
+}
+
+const LabelHOC = HOC(Label)
 const Person = (props) => <h4>{props.person.name}</h4>
 export default Wrapper
 
